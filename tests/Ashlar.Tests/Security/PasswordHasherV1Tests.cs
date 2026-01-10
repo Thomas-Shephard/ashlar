45db6053ae982415147288061dd4750be5f73635
Thomas Shephard <thomas@thomas-shephard.com>
2026-01-10 20:52:03 +0000

    fix GitHub copilot test suggestions

--- tests/Ashlar.Tests/Security/PasswordHasherV1Tests.cs
+++ tests/Ashlar.Tests/Security/PasswordHasherV1Tests.cs
@@ -4,6 +4,7 @@ namespace Ashlar.Tests.Security;
 
 public class PasswordHasherV1Tests
 {
+    private const int ExpectedHashLength = 1 + 16 + 32;
     private PasswordHasherV1 _hasher;
 
     [SetUp]
@@ -18,12 +19,32 @@ public class PasswordHasherV1Tests
         var password = "password123".AsSpan();
         var hash = _hasher.HashPassword(password);
 
-        Assert.That(hash, Has.Length.EqualTo(1 + 16 + 32));
+        Assert.That(hash, Has.Length.EqualTo(ExpectedHashLength));
         using (Assert.EnterMultipleScope())
         {
             Assert.That(hash[0], Is.EqualTo(_hasher.Version));
             Assert.That(_hasher.Version, Is.EqualTo(0x01));
         }
+    }
+
+    [Test]
+    public void HashPasswordWithEmptyPasswordShouldSucceed()
+    {
+        var password = "".AsSpan();
+        var hash = _hasher.HashPassword(password);
+
+        Assert.That(hash, Has.Length.EqualTo(ExpectedHashLength));
+        Assert.That(hash[0], Is.EqualTo(_hasher.Version));
+    }
+
+    [Test]
+    public void HashPasswordShouldProduceUniqueHashesForSamePassword()
+    {
+        var password = "password123".AsSpan();
+        var hash1 = _hasher.HashPassword(password);
+        var hash2 = _hasher.HashPassword(password);
+
+        Assert.That(hash1, Is.Not.EqualTo(hash2));
     }
 
     [Test]
@@ -61,22 +82,35 @@ public class PasswordHasherV1Tests
     }
 
     [Test]
-    public void VerifyPasswordWithNullHashShouldReturnFalse()
+    public void VerifyPasswordWithEmptyPasswordAndWrongHashShouldReturnFalse()
     {
         var password = "password123".AsSpan();
+        var emptyPassword = "".AsSpan();
+        var hash = _hasher.HashPassword(password);
 
-        var result = _hasher.VerifyPassword(password, null);
+        var result = _hasher.VerifyPassword(emptyPassword, hash);
 
         Assert.That(result, Is.False);
     }
 
     [Test]
-    public void HashPasswordShouldProduceUniqueHashesForSamePassword()
+    public void VerifyPasswordWithEmptyHashShouldReturnFalse()
     {
         var password = "password123".AsSpan();
-        var hash1 = _hasher.HashPassword(password);
-        var hash2 = _hasher.HashPassword(password);
 
-        Assert.That(hash1, Is.Not.EqualTo(hash2));
+        var result = _hasher.VerifyPassword(password, []);
+
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void VerifyPasswordWithEmptyPasswordShouldReturnTrueIfCorrect()
+    {
+        var password = "".AsSpan();
+        var hash = _hasher.HashPassword(password);
+
+        var result = _hasher.VerifyPassword(password, hash);
+
+        Assert.That(result, Is.True);
     }
 }
