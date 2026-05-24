587282ed3fbe0e1ea1987576f110651285fe23ba
Thomas Shephard <thomas@thomas-shephard.com>
2026-05-24 12:55:29 +0100

    refactor: split Ashlar service collection extensions by feature (#182)

---
+++ src/Ashlar/DependencyInjection/AshlarServiceCollectionExtensions.AccountFlows.cs
@@ -0,0 +1,156 @@
+// ReSharper disable CheckNamespace
+#pragma warning disable IDE0130
+namespace Microsoft.Extensions.DependencyInjection;
+#pragma warning restore IDE0130
+
+using Ashlar.Identity.Notifications;
+using Ashlar.Security.Encryption;
+using Ashlar.Security.Tokens;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+public static partial class AshlarServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers Ashlar's generic invitation and onboarding services.
+    /// </summary>
+    /// <param name="services">The services value.</param>
+    /// <param name="configure">The configure value.</param>
+    /// <returns>The service collection.</returns>
+    /// <remarks>
+    /// This method intentionally does not register <see cref="IInvitationRepository"/> or
+    /// <see cref="IIdentityRepository"/>. Applications should provide those dependencies explicitly,
+    /// such as by using Ashlar.Postgres or custom repository implementations.
+    /// </remarks>
+    public static IServiceCollection AddAshlarInvitations(
+        this IServiceCollection services,
+        Action<InvitationOptions>? configure = null)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddAshlarIdentity();
+        services.AddOptions();
+        if (configure != null)
+        {
+            services.Configure(configure);
+        }
+
+        services.TryAddScoped<IInvitationService, InvitationService>();
+        services.TryAddScoped<InvitationStoreContext>();
+        services.TryAddScoped<InvitationDependencies>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers Ashlar's generic bootstrap and first-admin setup services.
+    /// </summary>
+    /// <param name="services">The services value.</param>
+    /// <param name="configure">The configure value.</param>
+    /// <returns>The service collection.</returns>
+    public static IServiceCollection AddAshlarBootstrap(
+        this IServiceCollection services,
+        Action<BootstrapOptions>? configure = null)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddAshlarIdentity();
+        services.AddAshlarInvitations();
+        services.AddAshlarAuthorization();
+        services.AddOptions();
+        if (configure != null)
+        {
+            services.Configure(configure);
+        }
+
+        services.TryAddScoped<IBootstrapService, BootstrapService>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers Ashlar's email verification services.
+    /// </summary>
+    /// <param name="services">The services value.</param>
+    /// <param name="configure">The configure value.</param>
+    /// <returns>The service collection.</returns>
+    public static IServiceCollection AddAshlarEmailVerification(
+        this IServiceCollection services,
+        Action<EmailVerificationOptions>? configure = null)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddAshlarIdentity();
+        services.AddOptions();
+        if (configure != null)
+        {
+            services.Configure(configure);
+        }
+
+        services.TryAddScoped(provider => new EmailVerificationServiceDependencies(
+            provider.GetRequiredService<IdentityContext>(),
+            provider.GetRequiredService<SecureTokenContext>(),
+            provider.GetRequiredService<IdentityInfrastructureContext>(),
+            provider.GetRequiredService<IdentityAuditContext>(),
+            provider.GetService<IOptions<EmailVerificationOptions>>()));
+        services.TryAddScoped<IEmailVerificationService, EmailVerificationService>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers Ashlar's email change services.
+    /// </summary>
+    /// <param name="services">The services value.</param>
+    /// <param name="configure">The configure value.</param>
+    /// <returns>The service collection.</returns>
+    public static IServiceCollection AddAshlarEmailChange(
+        this IServiceCollection services,
+        Action<EmailChangeOptions>? configure = null)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddAshlarIdentity();
+        services.AddOptions();
+        if (configure != null)
+        {
+            services.Configure(configure);
+        }
+
+        services.TryAddScoped(provider => new EmailChangeDependencies(
+            provider.GetRequiredService<IdentityContext>(),
+            provider.GetRequiredService<SecureTokenContext>(),
+            provider.GetRequiredService<IdentityInfrastructureContext>(),
+            provider.GetRequiredService<IAuthenticationSessionRepository>(),
+            provider.GetRequiredService<ISecretProtector>(),
+            provider.GetRequiredService<IdentityAuditContext>()));
+        services.TryAddScoped<IEmailChangeService, EmailChangeService>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers Ashlar's generic security notification services.
+    /// </summary>
+    /// <param name="services">The services value.</param>
+    /// <param name="configure">The configure value.</param>
+    /// <returns>The service collection.</returns>
+    public static IServiceCollection AddAshlarSecurityNotifications(
+        this IServiceCollection services,
+        Action<SecurityNotificationOptions>? configure = null)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddAshlarIdentity();
+        services.AddOptions();
+        if (configure != null)
+        {
+            services.Configure(configure);
+        }
+
+        services.TryAddSingleton<ISecurityNotificationSuppressionStore, InMemorySecurityNotificationSuppressionStore>();
+        services.TryAddScoped<ISecurityNotificationService, SecurityNotificationService>();
+
+        return services;
+    }
+}
---
+++ src/Ashlar/DependencyInjection/AshlarServiceCollectionExtensions.EmailAuthentication.cs
@@ -0,0 +1,66 @@
+// ReSharper disable CheckNamespace
+#pragma warning disable IDE0130
+namespace Microsoft.Extensions.DependencyInjection;
+#pragma warning restore IDE0130
+
+using Ashlar.Identity.Providers.Email;
+using Ashlar.Security.Hashing;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+public static partial class AshlarServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers Ashlar's passwordless email code sign-in provider and issuing service.
+    /// </summary>
+    /// <param name="services">The services value.</param>
+    /// <param name="configure">The configure value.</param>
+    /// <returns>The service collection.</returns>
+    public static IServiceCollection AddAshlarEmailCodeSignIn(
+        this IServiceCollection services,
+        Action<EmailCodeSignInOptions>? configure = null)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddAshlarIdentity();
+        services.AddOptions();
+        if (configure != null)
+        {
+            services.Configure(configure);
+        }
+
+        services.TryAddEnumerable(ServiceDescriptor.Scoped<IAuthenticationProvider, EmailCodeAuthenticationProvider>());
+        services.TryAddScoped(provider => provider.GetServices<IAuthenticationProvider>().OfType<EmailCodeAuthenticationProvider>().First());
+        services.TryAddScoped<EmailCodeSignInDependencies>();
+        services.TryAddScoped<IEmailCodeSignInService, EmailCodeSignInService>();
+        services.TryAddEnumerable(ServiceDescriptor.Scoped<IPasswordHasher, PasswordHasherV1>());
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers Ashlar's magic-link email sign-in provider and issuing service.
+    /// </summary>
+    /// <param name="services">The services value.</param>
+    /// <param name="configure">The configure value.</param>
+    /// <returns>The service collection.</returns>
+    public static IServiceCollection AddAshlarMagicLinkSignIn(
+        this IServiceCollection services,
+        Action<MagicLinkSignInOptions>? configure = null)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddAshlarIdentity();
+        services.AddOptions();
+        if (configure != null)
+        {
+            services.Configure(configure);
+        }
+
+        services.TryAddEnumerable(ServiceDescriptor.Scoped<IAuthenticationProvider, MagicLinkAuthenticationProvider>());
+        services.TryAddScoped(provider => provider.GetServices<IAuthenticationProvider>().OfType<MagicLinkAuthenticationProvider>().First());
+        services.TryAddScoped<MagicLinkSignInDependencies>();
+        services.TryAddScoped<IMagicLinkSignInService, MagicLinkSignInService>();
+
+        return services;
+    }
+}
